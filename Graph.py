a58f327e3c04ed4fd3fa67c21970c8da9561f5ee
Louis Nyirongo <louisnyirongo73@gmail.com>
2023-10-31 00:05:46 +0200

    initialize empty graph and add node

--- Graph.py
+++ Graph.py
@@ -1,2 +1,19 @@
 class Graph:
+
+    def __init__(self) -> None:
+        self.nodes = []
+        self.graph = [[0 for _ in range(9)] for _ in range(9)]
+     
     
+    def add_node(self,node_name):
+        self.nodes.append(node_name)
+        index = self.nodes.index(node_name)
+        self.graph[index][index]=0
+    
+
+graph =Graph()
+graph.add_node("hey")
+graph.add_node("hello");
+
+
+print(graph.graph)
